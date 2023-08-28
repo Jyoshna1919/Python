d28bd42e3c306a97b2d31cd54086558e3ced6604
Jyoshna1919 <jyoshnamarolix@gmail.com>
2023-08-28 13:11:05 +0530

    string

---
+++ Assignment1.py
@@ -0,0 +1,13 @@
+#membership operator using in and not in
+a = "jyoshna"
+string_a = "hlo jyoshna"
+if (a in string_a):
+    print("True")
+else:
+    print("False")  # using in method
+a = "jyoshna"
+string_a = "hlo jyoshna"
+if (a not in string_a):
+    print("True")
+else:
+    print("False") # using not in method  
